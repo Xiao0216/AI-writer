5d45e356f91bb0bd34e383193470cbbf3b251763
jinshen <zhengze@ronglian.com>
2026-03-24 23:01:15 +0800

    fix: import novel documents module into novels module

--- apps/api/src/modules/novels/novels.module.ts
+++ apps/api/src/modules/novels/novels.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { NovelDocumentsModule } from '../novel-documents/novel-documents.module';
 import { NovelsController } from './novels.controller';
 import { NovelsService } from './novels.service';
 
 @Module({
+  imports: [NovelDocumentsModule],
   controllers: [NovelsController],
   providers: [NovelsService],
   exports: [NovelsService],
